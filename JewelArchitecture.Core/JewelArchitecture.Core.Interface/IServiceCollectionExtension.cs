6ff7cbda6d6ec7db43deb8797d254d79f47d2ca4
Emanuele Buldrini <emanuele.buldrini@gmail.com>
2024-12-03 21:37:47 +0100

    Use TryDecorate to manage exception when there are no services to decorate.

--- JewelArchitecture.Core/JewelArchitecture.Core.Interface/IServiceCollectionExtension.cs
+++ JewelArchitecture.Core/JewelArchitecture.Core.Interface/IServiceCollectionExtension.cs
@@ -13,27 +13,32 @@ namespace JewelArchitecture.Core.Interface;
 
 public static class IServiceCollectionExtension
 {
-    public static IServiceCollection AddJewelArchitecture(this IServiceCollection serviceCollection) =>
-         serviceCollection
-            .AddSingleton<IEventDispatcher, DIEventDispatcher>()
-            .AddSingleton(typeof(AggregateEventDispatcherService<,>))
-            .AddSingleton(typeof(ILockService<,>), typeof(InMemoryLockService<,>))
+    public static IServiceCollection AddJewelArchitecture(this IServiceCollection serviceCollection) {
+        serviceCollection
+           .AddSingleton<IEventDispatcher, DIEventDispatcher>()
+           .AddSingleton(typeof(AggregateEventDispatcherService<,>))
+           .AddSingleton(typeof(ILockService<,>), typeof(InMemoryLockService<,>))
 
-            .Scan(scan => scan.FromApplicationDependencies()
-                .AddClasses(classes => classes.AssignableToAny(typeof(ICommandHandler<>),
-                    typeof(IQueryHandler<,>), typeof(IEventHandler<>))
-                // Exclude decorators that should be added later to wrap decoratees.
-                .NotInNamespaceOf(typeof(AggregateEventDispatcherDecoratorBase<,,>))
-                )
-                .AsImplementedInterfaces()
-                .WithSingletonLifetime())
+           .Scan(scan => scan.FromApplicationDependencies()
+               .AddClasses(classes => classes.AssignableToAny(typeof(ICommandHandler<>),
+                   typeof(IQueryHandler<,>), typeof(IEventHandler<>))
+               // Exclude decorators that should be added later to wrap decoratees.
+               .NotInNamespaceOf(typeof(AggregateEventDispatcherDecoratorBase<,,>))
+               )
+               .AsImplementedInterfaces()
+               .WithSingletonLifetime())
 
+           // Add command decorators: should be registered after the command decoratees to wrap them.
+           .AddSingleton(typeof(AddOrReplaceAggregateCommandHandler<,>)) // Add the concrete type to avoid loops in the decorator.
+           .AddSingleton(typeof(IAddOrReplaceAggregateCommandHandler<,>), typeof(AddOrReplaceAggregateCommandEventDispatcher<,>))
+           .AddSingleton(typeof(RemoveAggregateCommandHandler<,>))// Add the concrete type to avoid loops in the decorator.
+           .AddSingleton(typeof(IRemoveAggregateCommandHandler<,>), typeof(RemoveAggregateCommandEventDispatcher<,>));
+        serviceCollection
+            .TryDecorate(typeof(IAggregateCommandHandler<,,>), typeof(AggregateCommandEventDispatcher<,,>));
+
+        return serviceCollection;
+    }
     // Add command decorators: should be registered after the command decoratees to wrap them.
-    .Decorate(typeof(IAggregateCommandHandler<,,>), typeof(AggregateCommandEventDispatcher<,,>))
-    .AddSingleton(typeof(AddOrReplaceAggregateCommandHandler<,>)) // Add the concrete type to avoid loops in the decorator.
-    .AddSingleton(typeof(IAddOrReplaceAggregateCommandHandler<,>), typeof(AddOrReplaceAggregateCommandEventDispatcher<,>))
-    .AddSingleton(typeof(RemoveAggregateCommandHandler<,>))// Add the concrete type to avoid loops in the decorator.
-    .AddSingleton(typeof(IRemoveAggregateCommandHandler<,>), typeof(RemoveAggregateCommandEventDispatcher<,>));
 
     public static IServiceCollection AddInMemoryJsonRepository(this IServiceCollection serviceCollection) =>
         serviceCollection.AddSingleton(typeof(IRepository<,>), typeof(InMemoryJsonRepository<,>))
